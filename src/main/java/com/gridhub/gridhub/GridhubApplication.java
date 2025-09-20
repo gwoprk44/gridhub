e94f92eb3afc68313822d7d88b4411d87dbfb49b
gwoprk98 <gwoprk98@gmail.com>
2025-09-20 10:03:48 +0900

    feat: 프로젝트 시작

---
+++ src/main/java/com/gridhub/gridhub/GridhubApplication.java
@@ -0,0 +1,13 @@
+package com.gridhub.gridhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GridhubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GridhubApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gridhub/gridhub/GridhubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gridhub.gridhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GridhubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
