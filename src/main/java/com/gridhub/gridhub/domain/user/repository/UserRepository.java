39b80019f140e1a5335db32cefab4f0869c46bdd
gwoprk98 <gwoprk98@gmail.com>
2025-09-20 12:00:07 +0900

    feat: 회원 리포지토리 구현

---
+++ src/main/java/com/gridhub/gridhub/domain/user/repository/UserRepository.java
@@ -0,0 +1,19 @@
+package com.gridhub.gridhub.domain.user.repository;
+
+import com.gridhub.gridhub.domain.user.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    // 이메일로 사용자를 찾는 메서드(로그인시 사용)
+    // 결과가 null일수 있음 -> 옵셔널로 감싸서 반환
+    Optional<User> findByEmail(String email);
+
+    // 이메일 중복 체크를 위한 메서드
+    boolean existsByEmail(String email);
+
+    // 닉네임 중복 체크를 위한 메서드
+    boolean existsByNickname(String nickname);
+}
