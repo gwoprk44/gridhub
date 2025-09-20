4750846e7000d2df6ef034870dc970915043a157
gwoprk98 <gwoprk98@gmail.com>
2025-09-20 12:30:36 +0900

    refactor: 커스텀 예외처리로 리팩토링

--- src/main/java/com/gridhub/gridhub/domain/user/service/UserService.java
+++ src/main/java/com/gridhub/gridhub/domain/user/service/UserService.java
@@ -3,6 +3,8 @@ package com.gridhub.gridhub.domain.user.service;
 
 import com.gridhub.gridhub.domain.user.dto.SignUpRequest;
 import com.gridhub.gridhub.domain.user.entity.User;
+import com.gridhub.gridhub.domain.user.exception.EmailAlreadyExistsException;
+import com.gridhub.gridhub.domain.user.exception.NicknameAlreadyExistsException;
 import com.gridhub.gridhub.domain.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -20,14 +22,13 @@ public class UserService {
     public void signUp(SignUpRequest request) {
         // 1. 이메일 중복 확인
         if (userRepository.existsByEmail(request.email())) {
-            // TODO: 추후 커스텀 예외로 변경
-            throw new IllegalArgumentException("이미 사용 중인 이메일입니다.");
+            throw new EmailAlreadyExistsException();
         }
 
         // 2. 닉네임 중복 확인
         if (userRepository.existsByNickname(request.nickname())) {
-            // TODO: 추후 커스텀 예외로 변경
-            throw new IllegalArgumentException("이미 사용 중인 닉네임입니다.");
+
+            throw new NicknameAlreadyExistsException();
         }
 
         // 3. DTO를 Entity로 변환 (비밀번호 암호화 포함)
