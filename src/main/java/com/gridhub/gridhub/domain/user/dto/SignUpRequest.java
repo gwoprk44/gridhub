f8737e8abb60928fe9190ddd696068e051fd16c8
gwoprk98 <gwoprk98@gmail.com>
2025-09-20 11:55:12 +0900

    feat: 사용자 인증/인가 기능 dto 구현

---
+++ src/main/java/com/gridhub/gridhub/domain/user/dto/SignUpRequest.java
@@ -0,0 +1,32 @@
+package com.gridhub.gridhub.domain.user.dto;
+
+import com.gridhub.gridhub.domain.user.entity.User;
+import com.gridhub.gridhub.domain.user.entity.UserRole;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+public record SignUpRequest(
+        @NotBlank(message = "이메일은 필수 입력 값입니다.")
+        @Email(message = "이메일 형식에 맞지 않습니다.")
+        String email,
+
+        @NotBlank(message = "비밀번호는 필수 입력 값입니다.")
+        @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*#?&])[A-Za-z\\d@$!%*#?&]{8,16}$",
+                message = "비밀번호는 영문, 숫자, 특수문자를 포함한 8~16자리여야 합니다.")
+        String password,
+
+        @NotBlank(message = "닉네임은 필수 입력 값입니다.")
+        String nickname
+) {
+    //DTO를 엔티티로 변환하는 메서드
+    public User toEntity(PasswordEncoder passwordEncoder) {
+        return User.builder()
+                .email(this.email)
+                .password(passwordEncoder.encode(this.password)) // 비밀번호 암호화
+                .nickname(this.nickname)
+                .role(UserRole.USER) // 기본 역할은 유저
+                .build();
+    }
+}
