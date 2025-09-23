daaef329e5258c54068e6e4f3a8ae466a8fa458f
gwoprk98 <gwoprk98@gmail.com>
2025-09-23 13:45:59 +0900

    feat: 게시글 목록 조회용 dto 구현

---
+++ src/main/java/com/gridhub/gridhub/domain/post/dto/PostSimpleResponse.java
@@ -0,0 +1,26 @@
+package com.gridhub.gridhub.domain.post.dto;
+
+import com.gridhub.gridhub.domain.post.entity.Post;
+import com.gridhub.gridhub.domain.post.entity.PostCategory;
+
+import java.time.LocalDateTime;
+
+public record PostSimpleResponse(
+        Long postId,
+        String title,
+        String authorNickname,
+        PostCategory category,
+        int viewCount,
+        LocalDateTime createdAt
+) {
+    public static  PostSimpleResponse from(Post post) {
+        return new PostSimpleResponse(
+                post.getId(),
+                post.getTitle(),
+                post.getAuthor().getNickname(),
+                post.getCategory(),
+                post.getViewCount(),
+                post.getCreatedAt()
+        );
+    }
+}
