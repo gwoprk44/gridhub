b045deae206d5291d97b5daca6d6177645cfdba9
gwoprk98 <gwoprk98@gmail.com>
2025-09-23 11:40:58 +0900

    feat: 카테고리 enum 추가

---
+++ src/main/java/com/gridhub/gridhub/domain/post/dto/PostCategory.java
@@ -0,0 +1,14 @@
+package com.gridhub.gridhub.domain.post.dto;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum PostCategory {
+    FREE("자유게시판"),
+    INFO("정보/기술"),
+    RUMOR("루머/이슈");
+
+    private final String title;
+}
