56d022b24bfa57c6b067e991f30aef01ba22de70
gwoprk98 <gwoprk98@gmail.com>
2025-09-23 13:51:35 +0900

    feat: 게시글 목록 조회 api 구현

--- src/main/java/com/gridhub/gridhub/domain/post/controller/PostController.java
+++ src/main/java/com/gridhub/gridhub/domain/post/controller/PostController.java
@@ -3,10 +3,15 @@ package com.gridhub.gridhub.domain.post.controller;
 import com.gridhub.gridhub.domain.post.dto.PostCreateRequest;
 import com.gridhub.gridhub.domain.post.dto.PostIdResponse;
 import com.gridhub.gridhub.domain.post.dto.PostResponse;
+import com.gridhub.gridhub.domain.post.dto.PostSimpleResponse;
 import com.gridhub.gridhub.domain.post.service.PostService;
 import com.gridhub.gridhub.global.security.UserDetailsImpl;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -38,5 +43,14 @@ public class PostController {
     public ResponseEntity<PostResponse> getPost(@PathVariable Long postId) {
         PostResponse postResponse = postService.getPost(postId);
         return ResponseEntity.ok(postResponse);
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<PostSimpleResponse>> getPostList(
+            @PageableDefault(size = 10, sort = "createdAt", direction = Sort.Direction.DESC)
+            Pageable pageable
+    ) {
+        Page<PostSimpleResponse> postList = postService.getPostList(pageable);
+        return ResponseEntity.ok(postList);
     }
 }
