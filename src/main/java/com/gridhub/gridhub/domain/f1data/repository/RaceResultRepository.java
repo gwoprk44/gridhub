b5a012bfadf57e7301b168e7451de4d5958abe56
gwoprk98 <gwoprk98@gmail.com>
2025-09-25 14:18:14 +0900

    refactor: 레이스 조회 메서드 추가

--- src/main/java/com/gridhub/gridhub/domain/f1data/repository/RaceResultRepository.java
+++ src/main/java/com/gridhub/gridhub/domain/f1data/repository/RaceResultRepository.java
@@ -1,7 +1,11 @@
 package com.gridhub.gridhub.domain.f1data.repository;
 
+import com.gridhub.gridhub.domain.f1data.entity.Race;
 import com.gridhub.gridhub.domain.f1data.entity.RaceResult;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface RaceResultRepository extends JpaRepository<RaceResult, Long> {
+    Optional<RaceResult> findByRace(Race race);
 }
