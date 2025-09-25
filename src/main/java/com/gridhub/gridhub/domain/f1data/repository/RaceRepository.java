0f3451c80cdde36f40c596d1957395ebe7eec942
gwoprk98 <gwoprk98@gmail.com>
2025-09-25 11:51:38 +0900

    refactor: 레이스 조회를 위한 메서드 추가

--- src/main/java/com/gridhub/gridhub/domain/f1data/repository/RaceRepository.java
+++ src/main/java/com/gridhub/gridhub/domain/f1data/repository/RaceRepository.java
@@ -3,5 +3,8 @@ package com.gridhub.gridhub.domain.f1data.repository;
 import com.gridhub.gridhub.domain.f1data.entity.Race;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface RaceRepository extends JpaRepository<Race, Long> {
+    List<Race> findByYearOrderByDateStartAsc(Integer year);
 }
