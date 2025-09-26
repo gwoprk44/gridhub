7e34bd314132753062a77938b4cf4ab13edfc0e7
gwoprk98 <gwoprk98@gmail.com>
2025-09-26 14:22:52 +0900

    refactor: 페치 조인 쿼리 추가 n+1 방지용

--- src/main/java/com/gridhub/gridhub/domain/f1data/repository/TeamRepository.java
+++ src/main/java/com/gridhub/gridhub/domain/f1data/repository/TeamRepository.java
@@ -2,6 +2,11 @@ package com.gridhub.gridhub.domain.f1data.repository;
 
 import com.gridhub.gridhub.domain.f1data.entity.Team;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface TeamRepository extends JpaRepository<Team, Long> {
+    @Query("SELECT DISTINCT t FROM Team t LEFT JOIN FETCH t.drivers")
+    List<Team> findAllWithDrivers();
 }
