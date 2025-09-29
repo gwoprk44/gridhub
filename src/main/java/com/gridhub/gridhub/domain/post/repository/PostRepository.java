d23a6a5feb24cdaff366cd15dbee34abec1d1930
gwoprk98 <gwoprk98@gmail.com>
2025-09-29 22:07:42 +0900

    refactor: 카테고리 조회 메서드 추가

--- src/main/java/com/gridhub/gridhub/domain/post/repository/PostRepository.java
+++ src/main/java/com/gridhub/gridhub/domain/post/repository/PostRepository.java
@@ -1,8 +1,14 @@
 package com.gridhub.gridhub.domain.post.repository;
 
 import com.gridhub.gridhub.domain.post.entity.Post;
+import com.gridhub.gridhub.domain.post.entity.PostCategory;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
     //TODO: 페이징, 검색 쿼리 추가 예정
+
+    // 특정 카테고리에 해당하는 게시글들을 페이징하여 조회
+    Page<Post> findByCategory(PostCategory category, Pageable pageable);
 }
