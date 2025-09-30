c39b772854c9a11714d19c026ff4830b83cdbd33
gwoprk98 <gwoprk98@gmail.com>
2025-09-30 10:27:41 +0900

    style: 주석 삭제

--- src/main/java/com/gridhub/gridhub/domain/post/repository/PostRepository.java
+++ src/main/java/com/gridhub/gridhub/domain/post/repository/PostRepository.java
@@ -7,7 +7,6 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
-    //TODO: 페이징, 검색 쿼리 추가 예정
 
     // 특정 카테고리에 해당하는 게시글들을 페이징하여 조회
     Page<Post> findByCategory(PostCategory category, Pageable pageable);
