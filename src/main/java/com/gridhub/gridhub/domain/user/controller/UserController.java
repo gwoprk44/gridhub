beb9ef65cdbf974f9c4141f927107260947bcb0a
gwoprk98 <gwoprk98@gmail.com>
2025-10-01 22:00:19 +0900

    refactor: 프로필 기능을 위한 api 구현

--- src/main/java/com/gridhub/gridhub/domain/user/controller/UserController.java
+++ src/main/java/com/gridhub/gridhub/domain/user/controller/UserController.java
@@ -1,17 +1,18 @@
 package com.gridhub.gridhub.domain.user.controller;
 
-import com.gridhub.gridhub.domain.user.dto.LoginRequest;
-import com.gridhub.gridhub.domain.user.dto.SignUpRequest;
-import com.gridhub.gridhub.domain.user.dto.TokenDto;
+import com.gridhub.gridhub.domain.user.dto.*;
 import com.gridhub.gridhub.domain.user.service.UserService;
+import com.gridhub.gridhub.global.security.UserDetailsImpl;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 @RestController
 @RequiredArgsConstructor
@@ -30,5 +31,21 @@ public class UserController {
     public ResponseEntity<TokenDto> login(@Valid @RequestBody LoginRequest request) {
         String token = userService.login(request);
         return ResponseEntity.ok(new TokenDto(token));
+    }
+
+    @GetMapping("/me")
+    public ResponseEntity<ProfileResponse> getMyProfile(@AuthenticationPrincipal UserDetailsImpl userDetails) {
+        ProfileResponse profile = userService.getMyProfile(userDetails.getUsername());
+        return ResponseEntity.ok(profile);
+    }
+
+    @PatchMapping(value = "/me", consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.MULTIPART_FORM_DATA_VALUE})
+    public ResponseEntity<Void> updateMyProfile(
+            @AuthenticationPrincipal UserDetailsImpl userDetails,
+            @RequestPart("request") @Valid ProfileUpdateRequest request,
+            @RequestPart(value = "image", required = false) MultipartFile profileImage
+    ) throws IOException {
+        userService.updateMyProfile(userDetails.getUsername(), request, profileImage);
+        return ResponseEntity.ok().build();
     }
 }
