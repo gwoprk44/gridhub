959940af6d18a5677b1b4883fde3d5940573637e
gwoprk98 <gwoprk98@gmail.com>
2025-10-01 10:31:41 +0900

    feat: EmitterRepository 구현체 구현

---
+++ src/main/java/com/gridhub/gridhub/infra/notification/MemoryEmitterRepository.java
@@ -0,0 +1,32 @@
+package com.gridhub.gridhub.infra.notification;
+
+import com.gridhub.gridhub.domain.notification.repository.EmitterRepository;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.stream.Collectors;
+
+public class MemoryEmitterRepository implements EmitterRepository {
+
+    // 동시성 문제 해결
+    private final Map<String, SseEmitter> emitters = new ConcurrentHashMap<>();
+
+    @Override
+    public SseEmitter save(String emitterId, SseEmitter sseEmitter) {
+        emitters.put(emitterId, sseEmitter);
+        return sseEmitter;
+    }
+
+    @Override
+    public void deleteById(String emitterId) {
+        emitters.remove(emitterId);
+    }
+
+    @Override
+    public Map<String, SseEmitter> findAllByUserId(String userId) {
+        return emitters.entrySet().stream()
+                .filter(entry -> entry.getKey().startsWith(userId))
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+    }
+}
