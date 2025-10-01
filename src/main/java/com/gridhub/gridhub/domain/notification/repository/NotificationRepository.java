433dcfe031015acee340ba5fa48f0a4ea972632a
gwoprk98 <gwoprk98@gmail.com>
2025-10-01 20:02:14 +0900

    refactor: 알림 목록 조회, 개수 조회를 위한 메서드 추가

--- src/main/java/com/gridhub/gridhub/domain/notification/repository/NotificationRepository.java
+++ src/main/java/com/gridhub/gridhub/domain/notification/repository/NotificationRepository.java
@@ -1,7 +1,16 @@
 package com.gridhub.gridhub.domain.notification.repository;
 
 import com.gridhub.gridhub.domain.notification.entity.Notification;
+import com.gridhub.gridhub.domain.user.entity.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface NotificationRepository extends JpaRepository<Notification, Long> {
+
+    // 특정 수신자의 알림 목록을 최신순으로 페이징하여 조회
+    Page<Notification> findAllByReceiverOrderByCreatedAtDesc(User receiver, Pageable pageable);
+
+    // 특정 수신자의 읽지 않은 알림 개수를 조회
+    long countByReceiverAndIsReadFalse(User receiver);
 }
