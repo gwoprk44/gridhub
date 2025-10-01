87dea229ff270b34d8f28df1de9e49d334193120
gwoprk98 <gwoprk98@gmail.com>
2025-10-01 10:20:13 +0900

    feat: 알림 기능을 위한 리포지토리 생성

---
+++ src/main/java/com/gridhub/gridhub/domain/notification/repository/NotificationRepository.java
@@ -0,0 +1,7 @@
+package com.gridhub.gridhub.domain.notification.repository;
+
+import com.gridhub.gridhub.domain.notification.entity.Notification;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface NotificationRepository extends JpaRepository<Notification, Long> {
+}
