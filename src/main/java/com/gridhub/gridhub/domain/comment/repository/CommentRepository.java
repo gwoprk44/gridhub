5232cd7cca1b9acf1bc45e3ea942c1347497ac56
gwoprk98 <gwoprk98@gmail.com>
2025-10-01 14:00:27 +0900

    refactor: 로직 수정

--- src/main/java/com/gridhub/gridhub/domain/comment/repository/CommentRepository.java
+++ src/main/java/com/gridhub/gridhub/domain/comment/repository/CommentRepository.java
@@ -9,6 +9,6 @@ import org.springframework.data.repository.query.Param;
 import java.util.List;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
-    @Query("SELECT c FROM Comment c LEFT JOIN FETCH c.author LEFT JOIN FETCH c.children WHERE c.post = :post AND c.parent IS NULL ORDER BY c.createdAt ASC")
+    @Query("SELECT DISTINCT c FROM Comment c LEFT JOIN FETCH c.children WHERE c.post = :post AND c.parent IS NULL ORDER BY c.createdAt ASC")
     List<Comment> findByPostWithChildren(@Param("post") Post post);
 }
