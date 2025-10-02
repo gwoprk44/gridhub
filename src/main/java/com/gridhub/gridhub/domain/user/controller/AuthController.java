e05480eacedaf52afa4e43f198ec3a900336f12d
gwoprk98 <gwoprk98@gmail.com>
2025-10-02 10:24:06 +0900

    refactor: 서비스 로직 리팩토링에 따른 api 리팩토링

--- src/main/java/com/gridhub/gridhub/domain/user/controller/AuthController.java
+++ src/main/java/com/gridhub/gridhub/domain/user/controller/AuthController.java
@@ -1,6 +1,7 @@
 package com.gridhub.gridhub.domain.user.controller;
 
 import com.gridhub.gridhub.domain.user.dto.*;
+import com.gridhub.gridhub.domain.user.service.AuthService;
 import com.gridhub.gridhub.domain.user.service.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -13,17 +14,17 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api/auth")
 public class AuthController {
 
-    private final UserService userService;
+    private final AuthService authService;
 
     @PostMapping("/signup")
     public ResponseEntity<Void> signUp(@Valid @RequestBody SignUpRequest request) {
-        userService.signUp(request);
+        authService.signUp(request);
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
     @PostMapping("/login")
     public ResponseEntity<TokenDto> login(@Valid @RequestBody LoginRequest request) {
-        String token = userService.login(request);
+        String token = authService.login(request);
         return ResponseEntity.ok(new TokenDto(token));
     }
 }
