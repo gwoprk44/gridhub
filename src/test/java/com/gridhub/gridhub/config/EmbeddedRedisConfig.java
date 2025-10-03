fca060bb4ab6387de7eeef6f44d0b483bce26fcd
gwoprk98 <gwoprk98@gmail.com>
2025-10-03 21:46:00 +0900

    rafactor: 로직 수정

---
+++ src/test/java/com/gridhub/gridhub/config/EmbeddedRedisConfig.java
@@ -0,0 +1,30 @@
+package com.gridhub.gridhub.config;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import redis.embedded.RedisServer;
+
+@Profile("test") // test 프로파일에서만 동작하도록 설정
+@Configuration
+public class EmbeddedRedisConfig {
+    @Value("${spring.data.redis.port}")
+    private int redisPort;
+
+    private RedisServer redisServer;
+
+    @PostConstruct
+    public void startRedis() {
+        redisServer = new RedisServer(redisPort);
+        redisServer.start();
+    }
+
+    @PreDestroy
+    public void stopRedis() {
+        if (redisServer != null) {
+            redisServer.stop();
+        }
+    }
+}
